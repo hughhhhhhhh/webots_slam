15a52430f4ca0cd4720ecf5ea9be3968a6ab4a1d
hehuang <he.huang1@rwth-aachen.de>
2019-05-06 11:00:29 +0200

    update the slam result exploration

---
+++ controllers/matlab_supervisor/imsimilar.m
@@ -0,0 +1,51 @@
+function value = imsimilar( count1,count2,type)
+%imsimilar---�������ƶ�
+%count1------ֱ��ͼ1
+%count2------ֱ��ͼ2
+%value-------���ƶ�
+% 
+% 1, input image
+% 
+% 2, grayscale
+% 
+% 3, normalize image size
+% 
+% 4, simplify the grayscale to reduce the amount of calculation, for example, all grayscales divided by 5
+% 
+% 5, calculate the average gray value average
+% 
+% 6. Compare the average value of the average gray value. If it is large, it is recorded as 1 and the small value is recorded as 0.
+% 
+% 7. Compare the fingerprints of the two images and calculate the similarity.
+
+N1 = length(count1);
+N2 = length(count2);
+
+if N1~=N2
+    fprintf('N1<>N2');
+    value = 0;
+    fprintf('ά�ȳ���');
+    return ;
+end
+
+
+result = 0;
+% type=1ʱ����ʽһ���㣬���򰴹�ʽ������
+if type==1
+for x = 1:N1
+    den = max(count1(x),count2(x));
+    if den == 0 
+        den = 1;
+    end
+    result = result+(1-abs(count1(x)-count2(x))/den);   
+end
+    value = 100*result/N1;
+else
+    for x = 1:N1
+       result = result+min(count1(x),count2(x)); 
+    end
+    value = result*100;
+end
+   % fprintf('���ƶ�Ϊ��%s%%\n',num2str(value));
+end
+
